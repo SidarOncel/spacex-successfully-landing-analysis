ca84e416b56f64d2328ab7226b01bdcc4f89d8b0
Sidar Oncel <oncelsidar@gmail.com>
2026-04-08 23:55:07 +0800

    Added dashboard file with tasks

---
+++ src/dashboard.py
@@ -0,0 +1,6 @@
+'''
+TASK 1: Add a Launch Site Drop-down Input Component
+TASK 2: Add a callback function to render success-pie-chart based on selected site dropdown
+TASK 3: Add a Range Slider to Select Payload
+TASK 4: Add a callback function to render the success-payload-scatter-chart scatter plot'''
+
