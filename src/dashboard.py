b973253ea937ef312b4d716e74522dfed5c080aa
Sidar Oncel <oncelsidar@gmail.com>
2026-04-09 21:39:25 +0800

    initialized the dahsboard file

--- src/dashboard.py
+++ src/dashboard.py
@@ -4,3 +4,4 @@ TASK 2: Add a callback function to render success-pie-chart based on selected si
 TASK 3: Add a Range Slider to Select Payload
 TASK 4: Add a callback function to render the success-payload-scatter-chart scatter plot'''
 
+# Install required libraries "pip install pandas dash"
